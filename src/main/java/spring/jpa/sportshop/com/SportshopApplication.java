0e938770cb3d6a4324a6abd8714116425087ce88
tonysanes <tony.ta.sanchez@gmail.com>
2020-06-17 15:16:46 -0500

    iniciar

---
+++ src/main/java/spring/jpa/sportshop/com/SportshopApplication.java
@@ -0,0 +1,13 @@
+package spring.jpa.sportshop.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/jpa/sportshop/com/SportshopApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.jpa.sportshop.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
